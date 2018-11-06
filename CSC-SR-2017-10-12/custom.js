53f4e63b41fce1851ebf9b3ce98840ecea7728e5
Juha Lento <juha.lento@csc.fi>
2018-11-06 20:57:33 +0200

    Moved Org-Reveal slides to miljoonalaatikko

--- CSC-SR-2017-10-12/custom.js
+++
@@ -1,8 +0,0 @@
-// White, fixed size slide background
-
-[].forEach.call(document.querySelectorAll('section'), function (e) {
-    e.setAttribute("data-background-size", "contain");
-    e.setAttribute("data-background", "whitebg.svg");
-});
-
-//Reveal.sync();
