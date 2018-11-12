4360fea4e585cbc9bd2eede2b807c72a812ff3c5
Juha Lento <juha.lento@csc.fi>
2018-11-13 00:56:27 +0200

    Fixed aspect ratio and font scaling

--- _includes/majestic.js
+++ _includes/majestic.js
@@ -1,14 +1,31 @@
 var currentSlide = 0;
-const splitBefore = "H2";
+
+function resizeRem() {
+    document.documentElement.style.fontSize =
+        document.body.firstChild.offsetWidth/80 + "px";
+}
+
+function hide(element) {
+    element.style.display = "none";
+}
+
+function show(element) {
+    element.style.display = "block";
+}
+
+function splitBefore(tag) {
+    return ["H1", "H2", "H3"]
+        .some(function(splitat){return splitat === tag;});
+}
 
 function init() {
     var body = document.body;
     var islide = currentSlide;
     body.insertBefore(document.createElement("div"),
                       body.childNodes[0]);
-    body.childNodes[0].style.display = "none";
+    hide(body.childNodes[0]);
     while (body.childNodes.length > islide + 1) {
-        if (body.childNodes[islide + 1].tagName === splitBefore) {
+        if (splitBefore(body.childNodes[islide + 1].tagName)) {
             body.insertBefore(body.childNodes[0].cloneNode(false),
                               body.childNodes[islide].nextSibling);
             islide++;
@@ -16,8 +33,9 @@ function init() {
         body.childNodes[islide]
             .appendChild(body.childNodes[islide + 1]);
     }
-    body.style.display = "block";
-    body.childNodes[0].style.display = "block";
+    show(body);
+    show(body.childNodes[0]);
+    resizeRem();
 }
 
 function change(event) {
@@ -25,14 +43,14 @@ function change(event) {
     var key = event.charCode || event.keyCode;
     if ((key === 37 || key === 38) &&
         body.childNodes[currentSlide].previousSibling) {
-        body.childNodes[currentSlide].style.display = "none";
+        hide(body.childNodes[currentSlide]);
         currentSlide--;
-        body.childNodes[currentSlide].style.display = "block";
+        show(body.childNodes[currentSlide]);
     }
     if ((key === 39 || key === 40) &&
         body.childNodes[currentSlide].nextSibling) {
-        body.childNodes[currentSlide].style.display = "none";
+        hide(body.childNodes[currentSlide]);
         currentSlide++;
-        body.childNodes[currentSlide].style.display = "block";
+        show(body.childNodes[currentSlide]);
     }
 }
